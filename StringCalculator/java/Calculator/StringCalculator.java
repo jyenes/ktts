e790e05fafc26e3b171fcaecbee33329af152ded
jyenes <javi.yenes@gmail.com>
2013-12-05 21:48:01 +0000

    adding string calculator sources

---
+++ StringCalculator/java/Calculator/StringCalculator.java
@@ -0,0 +1,33 @@
+package Calculator;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * TDD Kt string calculator
+ * @author jyenes
+ *
+ */
+public class StringCalculator {
+
+	
+	
+	public String add(String numbers){
+		String result = numbers;
+		if(numbers.compareTo("")!=0){
+			Pattern pattern = Pattern.compile("-?\\d+");
+			Matcher matcher = pattern.matcher(numbers);
+			int sum =0 ;
+	        while(matcher.find()){
+	        	sum += Integer.valueOf(matcher.group());
+	        }
+	        result = sum + "";
+	        if (result.compareTo("0") == 0 && !matcher.matches()) {
+	        	result = "";
+	        }
+		}
+		
+		return result;
+	}
+		
+}
---
+++ StringCalculator/test/Calculator/StringCalculatorTest.java
@@ -0,0 +1,70 @@
+package Calculator;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+import Calculator.StringCalculator;
+
+
+
+/**
+ * TDD Kt string calculator
+ * @author yenesj
+ *
+ */
+public class StringCalculatorTest {
+	StringCalculator stringCalculator;
+	
+	@Before	
+	public void setUp(){
+		stringCalculator = new StringCalculator();
+		
+	}
+	
+	@Test
+	public void testAddEmpty() {
+		assertEquals( "",stringCalculator.add(""));
+	}
+	
+	@Test
+	public void testAddOneNumber() {
+		assertEquals("1", stringCalculator.add("1"));
+		assertEquals("2", stringCalculator.add("2"));
+		assertEquals("3", stringCalculator.add("3"));
+	}
+	
+	@Test
+	public void testAddTwoNumbers() {
+		assertEquals( "3", stringCalculator.add("1,2"));
+		assertEquals( "2010", stringCalculator.add("10,2000"));
+		assertEquals( "1", stringCalculator.add("10,-9"));
+	}
+	
+	@Test
+	public void testAddMoreThanTwoNumbers() {
+		assertEquals( "6", stringCalculator.add("1,2,3"));
+		assertEquals( "243", stringCalculator.add("10,200,33"));
+		assertEquals( "0", stringCalculator.add("1,2,-3"));
+		
+	}
+
+	@Test
+	public void testAddAllowingNewLines() {
+		assertEquals( "6", stringCalculator.add("1\n2,3"));
+		assertEquals( "1", stringCalculator.add("1,\n"));
+		assertEquals( "2000001", stringCalculator.add("1,\n2000000"));
+	}
+	
+	@Test
+	public void testAddAllowingAnyDelimiter() {
+		assertEquals( "3", stringCalculator.add("//;\n1;2"));
+	}
+	
+	@Test
+	public void testAddHandleNoNumbers() {
+		assertEquals( "", stringCalculator.add(";"));
+		assertEquals( "", stringCalculator.add(";;"));
+		assertEquals( "", stringCalculator.add(";\\n;"));
+	}
+}
