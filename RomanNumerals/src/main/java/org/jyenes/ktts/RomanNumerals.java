0e6bd25bd7d9dc0babbbd461aa43943b099f2b2e
jyenes <javi.yenes@gmail.com>
2013-12-08 09:07:55 +0000

    refactor

--- RomanNumerals/src/main/java/org/jyenes/ktts/RomanNumerals.java
+++ RomanNumerals/src/main/java/org/jyenes/ktts/RomanNumerals.java
@@ -1,30 +1,19 @@
 package org.jyenes.ktts;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 
 
 public class RomanNumerals {
 
-	private static final int[] decimals = {10,9,5,4};
-	private static final String[] romans = {"X", "IX", "V", "IV"};
+	private static final int[] decimals = {1000,900,500,400,100,90,50,40,10,9,5,4,1};
+	private static final String[] romans = {"M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"};
 
 	public String conver(int dec) {
 
-		
 		StringBuilder result = null;
 		int rest = dec;
 		result = new StringBuilder();
 		for(int i = 0; i< decimals.length; i++){
 			rest = appendRoman(result, rest, decimals[i], romans[i]);
-			
-		}
-
-
-		for (int i = 0; i < rest; i++) {
-			result.append("I");
 		}
 		return result.toString();
 
@@ -33,9 +22,9 @@ public class RomanNumerals {
 	private int appendRoman(StringBuilder builder, int rest, int val, String roman) {
 		
 		int result = rest;
-		if (result >= val) {
-			result -= val;
+		while (result >= val) {
 			builder.append(roman);
+			result -= val;
 		}
 		return result;
 	}
